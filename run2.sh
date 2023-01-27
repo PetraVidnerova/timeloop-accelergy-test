5328fb74429cee5d2f90ff0d6742148b6dfb4dfd
Petra Vidnerova <petra.vidnerova@gmail.com>
2023-01-27 16:00:33 +0100

    use simplearch

--- run2.sh
+++ run2.sh
@@ -7,7 +7,8 @@ NET=test
 for LAYER in `seq 192 8 1024`
 do
     echo "*** $LAYER ***"
-    ${DIR}/timeloop-mapper eyeriss_like/mapper/mapper_tutorial.yaml  eyeriss_like/arch/eyeriss_like.yaml eyeriss_like/arch/components/* eyeriss_like/constraints/eyeriss_like_arch_constraints.yaml ${NET}_layer_${LAYER}.yaml > output/${NET}_tutorial_layer${LAYER}.txt 
+    #    ${DIR}/timeloop-mapper eyeriss_like/mapper/mapper_tutorial.yaml  eyeriss_like/arch/eyeriss_like.yaml eyeriss_like/arch/components/* eyeriss_like/constraints/eyeriss_like_arch_constraints.yaml ${NET}_layer_${LAYER}.yaml > output/${NET}_tutorial_layer${LAYER}.txt
+        ${DIR}/timeloop-mapper eyeriss_like/mapper/mapper_tutorial.yaml  simplearch.yaml ${NET}_layer_${LAYER}.yaml > output/${NET}_tutorial_layer${LAYER}.txt 
     mv timeloop-mapper.stats.txt output/${NET}_tutorial_layer${LAYER}.stats
 done
 
