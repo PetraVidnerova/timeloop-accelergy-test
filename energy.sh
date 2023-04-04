084169c19795f046975a6d9ce326a708a8e35407
Petra Vidnerova <petra.vidnerova@gmail.com>
2023-04-04 12:10:08 +0200

    obsolete files deleted

--- energy.sh
+++
@@ -1,7 +0,0 @@
-for I in `seq 8 8 1024`;
-do
-    echo -n "$I;"
-    ENERGY=`cat output/test_tutorial_layer$I.stats | grep "^Energy" | cut -d" " -f2`
-    echo -n "$ENERGY;"
-    python3 jirka_engergy.py test_layer_${I}.yaml
-done
--- energy_alex.sh
+++
@@ -1,9 +0,0 @@
-NET="alexnet"
-
-for I in `seq 1 8`;
-do
-    echo -n "$I;"
-    ENERGY=`cat output/${NET}_tutorial_layer$I.stats | grep "^Energy" | cut -d" " -f2`
-    echo -n "$ENERGY;"
-    python3 jirka_engergy.py ${NET}_layer${I}.yaml
-done
